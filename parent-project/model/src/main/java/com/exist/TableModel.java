c6eb271f49dabfad0b5494924170842b0fcd3881
Christelle Ridad <sobbinginmycupofcoffee@gmail.com>
2025-02-03 18:49:53 +0800

    Maven activities

---
+++ parent-project/model/src/main/java/com/exist/TableModel.java
@@ -0,0 +1,39 @@
+package com.exist; 
+import java.util.ArrayList;
+import java.util.List;
+public class TableModel {
+
+    // each row (also a list) IS a list of key-value pairs 
+
+    // row shud be a seperate class by itself
+    // public class table
+    // public class pair
+    // public class row
+    private List<List<Pair>> rows;
+
+    // constructor to initialize the Table with rows
+    public TableModel() {
+        this.rows = new ArrayList<>();
+    }
+
+    // getter for row
+    public List<List<Pair>> getRows() {
+        return rows;
+    }
+
+    public void addRow(List<Pair> row) {
+        rows.add(row); 
+    }
+
+    
+    // Pair class to represent a key-value pair
+    public static class Pair {
+        String key;
+        String value;
+
+        Pair(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
